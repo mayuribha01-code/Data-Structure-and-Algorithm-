28643ed21667352da258741dd977e03bcf2b1d51
mayuribha01-code <mayuribha01@gmail.com>
2026-07-21 23:28:55 +0530

    Learning String and it's Operations

---
+++ string.py
@@ -0,0 +1,10 @@
+# \n : Next line 
+# \t : Tab spacing
+# \\ : Backslash
+# \" : Adding quote in quoted statement
+
+print("Hello everyone, My name Mayuri \nHow are you?")
+print('Mayuri:\t20')
+print("new\\old")
+print('Mayuri\'s phone')
+print("Hey!! \"Mayuri\"")
